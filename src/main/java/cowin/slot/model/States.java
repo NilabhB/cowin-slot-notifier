c47ccb91d6c71735b34b3c9f419621d89e2fbf73
Nilabh Nayan Borthakur <borthakur.nilabh@gmail.com>
2021-05-12 12:27:58 +0530

    Added States & Districts

---
+++ src/main/java/cowin/slot/model/States.java
@@ -0,0 +1,15 @@
+package cowin.slot.model;
+
+import java.util.List;
+
+public class States {
+    private final List<State> states;
+
+    public States(List<State> states) {
+        this.states = states;
+    }
+
+    public List<State> getStates() {
+        return states;
+    }
+}
